3f86452a5cb095f17329ca54cabec313f683ad4b
Indrajeet <indrajeet95@gmail.com>
2018-08-18 14:38:39 -0400

    linked list

--- 876_Middle_of_the_Linked_List.cpp
+++ 876_Middle_of_the_Linked_List.cpp
@@ -22,3 +22,28 @@ public:
             return temp->next;
     }
 };
+
+///**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* p1 = head;
+        ListNode* p2 = head;
+        while(p2->next!=NULL && p2->next->next!=NULL)
+        {
+            p1 = p1->next;
+            p2 = p2->next->next;
+        }
+        if(p2->next == NULL)
+            return p1;
+        else
+            return p1->next;
+    }
+};
