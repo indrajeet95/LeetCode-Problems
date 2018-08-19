82a8f1bf6bc3916d96de4c8468d32f2ca297fc30
Indrajeet <indrajeet95@gmail.com>
2018-08-19 12:04:07 -0400

    LL

--- 445_Add_Two_Numbers_II.cpp
+++ 445_Add_Two_Numbers_II.cpp
@@ -56,4 +56,54 @@ public:
         }
         return prev;
     }
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> s1;
+        stack<int> s2;
+        while(l1 != NULL)
+        {
+            s1.push(l1->val);
+            l1 = l1->next;
+        }
+        while(l2 != NULL)
+        {
+            s2.push(l2->val);
+            l2 = l2->next;
+        }
+        int sum = 0;
+        ListNode* front = new ListNode(0);
+        while(s1.size()!=0 || s2.size()!=0)
+        {
+            if(s1.size() != 0)
+            {
+                sum += s1.top();
+                s1.pop();                
+            }
+            if(s2.size() != 0)
+            {
+                sum += s2.top();
+                s2.pop();            
+            }
+            front->val = sum%10;
+            ListNode* head = new ListNode(sum/10);
+            head->next = front;
+            front = head;
+            sum = sum/10;
+        }
+        if(front->val == 0)
+            return front->next;
+        else
+            return front;
+    }
 };
