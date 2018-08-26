05c9422abe1d286a2b5a36b97320d81d143a1f98
Indrajeet <indrajeet95@gmail.com>
2018-08-26 17:46:04 -0400

    hard

--- 42_Trapping_Rain_Water.cpp
+++ 42_Trapping_Rain_Water.cpp
@@ -17,3 +17,22 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans = 0;
+        int size = height.size();
+        vector<int> lmax(size);
+        vector<int> rmax(size);
+        lmax[0] = height[0];
+        rmax[size-1] = height[size-1];
+        for(int i=1;i<size;i++)
+            lmax[i] = max(lmax[i-1],height[i]);
+        for(int i=size-2;i>=0;i--)
+            rmax[i] = max(rmax[i+1],height[i]);
+        for(int i=1;i<size-1;i++)
+            ans += min(lmax[i],rmax[i]) - height[i];
+        return ans;
+    }
+};
