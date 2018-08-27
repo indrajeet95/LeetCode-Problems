ccf9369e45ce80a16e5591277764efecb0fce1fb
Indrajeet <indrajeet95@gmail.com>
2018-08-27 08:16:27 -0400

    hard

--- 42_Trapping_Rain_Water.cpp
+++ 42_Trapping_Rain_Water.cpp
@@ -36,3 +36,33 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int lmax = 0,rmax = 0;
+        int left = 0;
+        int right = height.size()-1;
+        int ans = 0;
+        while(left < right)
+        {
+            if(height[left] < height[right])
+            {
+                if(height[left]>=lmax)
+                    lmax = height[left];
+                else
+                    ans += lmax - height[left];
+                left++;
+            }
+            else
+            {
+                if(height[right]>=rmax)
+                    rmax = height[right];
+                else
+                    ans += rmax - height[right];
+                right--;
+            }
+        }
+        return ans;
+    }
+};
