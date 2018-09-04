999126263101a63b6a5e15f6d599758f802b377b
Indrajeet <indrajeet95@gmail.com>
2018-09-03 23:48:21 -0400

    easy

---
+++ 167_Two_Sum_II_-_Input_array_is_sorted.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> res;
+        for(int i=0;i<numbers.size();i++)
+        {
+            for(int j = i+1;j<numbers.size();j++)
+            {
+                if(numbers[i] + numbers[j] == target)
+                {
+                    res.push_back(i+1);
+                    res.push_back(j+1);
+                }
+            }           
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> res;
+        map<int,int> maps;
+        for(int i=0;i<numbers.size();i++)
+            maps[numbers[i]] = i;
+        for(int i=0;i<numbers.size();i++)
+            if(maps.count(target - numbers[i])>0 && maps[target - numbers[i]] != i)
+            {
+                res.push_back(i+1);
+                res.push_back(maps[target-numbers[i]]+1);
+                break;
+            }
+        return res;
+    }
+};
