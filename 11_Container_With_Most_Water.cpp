6c906db8ad7ea5632eab483ad01e440930604195
Indrajeet <indrajeet95@gmail.com>
2018-09-04 20:45:25 -0400

    easy

--- 11_Container_With_Most_Water.cpp
+++ 11_Container_With_Most_Water.cpp
@@ -11,3 +11,21 @@ public:
         return maxarea;
     }
 };
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxarea = 0;
+        int l = 0;
+        int r = height.size()-1;
+        while(l<r)
+        {
+            maxarea = max(maxarea,(r-l)*min(height[l],height[r]));
+            if(height[l]<height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+    }
+};
