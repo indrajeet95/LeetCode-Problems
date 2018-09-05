2ff81bdb72ff6b9ca3bdb98f55e3b99aa119d5cd
Indrajeet <indrajeet95@gmail.com>
2018-09-04 23:53:21 -0400

    easy

---
+++ 532_K-diff_Pairs_in_an_Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        if(k < 0) return 0;
+        unordered_map<int,int> m;
+        for(int i = 0; i < nums.size(); ++i)
+           ++m[nums[i]];
+        int res = 0;
+        if(k != 0) {
+            for(auto it = m.begin(); it != m.end(); ++it)
+               if(m.find(it->first+k) != m.end())
+                   ++res;
+        } else {
+            for(auto it = m.begin(); it != m.end(); ++it)
+               if(it->second > 1)
+                   ++res;
+        }
+        return res;
+    }
+};
