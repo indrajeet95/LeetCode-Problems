e1f83c2dd267eeea1184bcf7a999eea2e70885f6
Indrajeet Saravanan <indrajeet95@gmail.com>
2018-09-06 12:31:38 -0400

    one pass solution

--- 75_Sort_Colors.cpp
+++ 75_Sort_Colors.cpp
@@ -24,3 +24,28 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i=0;
+        int m=0;
+        int k = nums.size()-1;
+        while(m<=k)
+        {
+            if(nums[m] == 0)
+            {
+                swap(nums[i],nums[m]);
+                i++;
+                m++;
+            }
+            else if(nums[m] == 2)
+            {
+                swap(nums[m],nums[k]);
+                k--;
+            }
+            else
+                m++;
+        }
+    }
+};
