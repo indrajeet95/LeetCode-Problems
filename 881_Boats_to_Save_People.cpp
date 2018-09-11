57e5e9f6869139207e0e030b08a4a990b80724e4
Indrajeet <indrajeet95@gmail.com>
2018-09-11 12:18:08 -0400

    two pointers

---
+++ 881_Boats_to_Save_People.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int i = 0;
+        int j = people.size()-1;
+        int boats  = 0;
+        while(i<=j)
+        {
+            if(people[i] + people[j] <= limit)
+            {
+                boats++;
+                i++;
+                j--;
+            }
+            else if(people[j] <= limit)
+            {
+                boats++;
+                j--;            
+            }
+        }
+        return boats;
+    }
+};
