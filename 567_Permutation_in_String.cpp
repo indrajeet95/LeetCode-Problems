186c26cd51897502888235821da13cb47670dca0
Indrajeet <indrajeet95@gmail.com>
2018-09-11 19:20:22 -0400

    two pointers

--- 567_Permutation_in_String.cpp
+++ 567_Permutation_in_String.cpp
@@ -33,3 +33,18 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        sort(s1.begin(),s1.end());
+        for(int i=0;i<=s2.length()-s1.length();i++)
+        {
+            string temp = s2.substr(i,s1.length());
+            sort(temp.begin(),temp.end());
+            if(temp.compare(s1) == 0)
+                return true;
+        }
+        return false;
+    }
+};
