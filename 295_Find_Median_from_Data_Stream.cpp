4c330421fb84c9f0777493fb5a153a061e3d15da
Indrajeet <indrajeet95@gmail.com>
2018-10-15 12:35:38 -0400

    pelase

--- 295_Find_Median_from_Data_Stream.cpp
+++ 295_Find_Median_from_Data_Stream.cpp
@@ -27,3 +27,47 @@ public:
             return lo.top();
     }
 };
+
+
+class MedianFinder {
+public:
+    multiset<int> set;
+    multiset<int>::iterator left,right;
+    /** initialize your data structure here. */
+    MedianFinder() {
+        //left = set.end();
+        //right = set.end();
+    }
+    
+    void addNum(int num) {
+        const size_t n = set.size();
+        set.insert(num);
+        if(n == 0)
+        {
+            left = right = set.begin();
+        }
+        else if(n%2 == 1)
+        {
+            if(num < *left)
+                left--;
+            else
+                right++;
+        }
+        else
+        {
+            if(num>*left && num <*right)
+            {
+                left++;
+                right--;
+            }
+            else if(num>=*right)
+                left++;
+            else
+                left = --right;
+        }
+    }
+    
+    double findMedian() {
+        return (double)(*left + *right)/2;
+    }
+};
