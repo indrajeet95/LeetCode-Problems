3bcae1e0e32f97de36c2d0e693e5d33be29f882a
Indrajeet <indrajeet95@gmail.com>
2018-10-26 02:42:10 -0400

    dfs

--- 56_Merge_Intervals.cpp
+++ 56_Merge_Intervals.cpp
@@ -31,3 +31,69 @@ public:
         return res;
     }
 };
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+
+class Solution {
+    struct compare{
+    bool operator() (Interval a, Interval b){
+    return (a.start < b.start);
+    }
+};
+    public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        vector<Interval> res;
+        int size = intervals.size();
+        if (size <= 0)
+            return res;
+        sort(intervals.begin(),intervals.end(), compare());
+        res.push_back(intervals[0]);
+        for(int i=1;i<size;i++)
+        {
+            if(res.back().end < intervals[i].start)
+                res.push_back(intervals[i]);
+            else
+                res.back().end = max(res.back().end,intervals[i].end);
+        }
+        return res;
+    }
+};
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+/*struct compare {
+  bool operator() (Interval a, Interval b)
+  {
+      return a.start < b.start;
+  }
+};
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        vector<Interval> res;
+        sort(intervals.begin(), intervals.end(), compare);
+        res.push_back(intervals[0]);
+        for(int i=1; i< intervals.size(); i++)
+        {
+            if(intervals[i].start > res.back().end)
+                res.push_back(intervals[i]);
+            else
+                res.back().end = max(res.back().end, intervals[i].end);
+        }
+    }
+};
