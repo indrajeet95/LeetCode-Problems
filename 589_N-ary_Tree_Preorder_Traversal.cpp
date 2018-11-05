b91d5d063f5ef371c0692369fc4114f55db7deeb
Indrajeet <indrajeet95@gmail.com>
2018-11-04 20:35:05 -0500

    here

--- 589_N-ary_Tree_Preorder_Traversal.cpp
+++ 589_N-ary_Tree_Preorder_Traversal.cpp
@@ -29,3 +29,38 @@ public:
             helper(child, holder);
     }
 };
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> res;
+        if(root == NULL)
+            return res;
+        stack<Node*> s;
+        s.push(root);
+        while(s.size() != 0)
+        {
+            Node* curr = s.top();
+            s.pop();
+            for(int i = curr->children.size() - 1; i>=0; i--)
+                s.push(curr->children[i]);
+            res.push_back(curr->val);
+        }
+        return res;
+    }
+};
