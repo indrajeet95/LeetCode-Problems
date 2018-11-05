2ac83668be58ce546d90e0a473d6d826ce933fc3
Indrajeet <indrajeet95@gmail.com>
2018-11-05 09:35:54 -0500

    yo

--- 590_N-ary_Tree_Postorder_Traversal.cpp
+++ 590_N-ary_Tree_Postorder_Traversal.cpp
@@ -29,3 +29,39 @@ public:
         res.push_back(root->val);
     }
 };
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> res;
+        if(root == NULL)
+            return res;
+        stack<Node*> s;
+        s.push(root);
+        while(s.size() != 0)
+        {
+            Node* curr = s.top();
+            s.pop();
+            for(auto child : curr->children)
+                s.push(child);
+            res.push_back(curr->val);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
