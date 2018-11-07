e6a35489f24ec418b2424fdbb6de526919333d80
Indrajeet <indrajeet95@gmail.com>
2018-11-07 17:04:06 -0500

    herer

---
+++ 110_Balanced_Binary_Tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL)
+            return true;
+        if(root->left == NULL && root->right == NULL)
+            return true;
+        int hl = height(root->left);
+        int hr = height(root->right);
+        if(hl - hr > 1 || hr - hl > 1)
+            return false;
+        else
+            return isBalanced(root->left) && isBalanced(root->right);
+    }
+    
+    int height(TreeNode* root)
+    {
+        if(root == NULL)
+            return 0;
+        return 1 + max(height(root->left), height(root->right));
+    }
+};
