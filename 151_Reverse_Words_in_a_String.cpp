1fd25432140389516ad076b525cbd175e5cf57d7
Indrajeet <indrajeet95@gmail.com>
2018-11-10 14:38:13 -0500

    micro

--- 151_Reverse_Words_in_a_String.cpp
+++ 151_Reverse_Words_in_a_String.cpp
@@ -15,3 +15,42 @@ public:
         s.pop_back();
     }
 };
+
+class Solution {
+public:
+    void reverseWords(string &s) {
+        reverse(s.begin(),s.end());
+        auto i = s.begin(), j = i;
+        while(i != s.end())
+        {
+            if(*i != ' ')
+            {
+                j = i+1;
+                while(*j != ' ' && j != s.end())
+                    j++;
+                reverse(i,j);
+                i = j;
+                if(i == s.end() || ++i == s.end())
+                    break;
+            }
+            else
+                i = s.erase(i);
+        }
+        if(*(s.rbegin()) == ' ')
+            s.pop_back();
+    }
+};
+/*
+void reverseWords(string &s) {
+      reverse(s.begin(), s.end());
+      auto i = s.begin(), j = i;
+      while (i != s.end()) {
+        if (*i != ' ') {
+          for (j = i + 1; j != s.end() && *j != ' '; j++); 
+          reverse(i, j); i = j; 
+          if (i == s.end() || ++i == s.end()) break;
+        }
+        else i = s.erase(i);
+      }
+      if (*(s.rbegin()) == ' ') s.pop_back();
+    }
