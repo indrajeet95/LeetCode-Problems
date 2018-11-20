4d3708085d73064754e6905bd81f2f6066b3b497
Indrajeet <indrajeet95@gmail.com>
2018-11-20 09:17:03 -0500

    here

--- 713_Subarray_Product_Less_Than_K.cpp
+++ 713_Subarray_Product_Less_Than_K.cpp
@@ -16,3 +16,27 @@ public:
         return count;
     }
 };
+
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int product = 1;
+        int count = 0;
+        int n = nums.size();
+        int i =0;
+        int j = 0;
+        while(j<n)
+        {
+            product *= nums[j];
+            while(i<=j && product >= k)
+            {
+                product = product/nums[i];
+                i++;
+            }
+            count += j - i + 1;
+            j++;
+        }
+        return count;
+    }
+};
