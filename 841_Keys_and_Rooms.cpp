3048e78d666a0694b1da7263342f9aa994285402
Indrajeet <indrajeet95@gmail.com>
2018-12-16 17:02:29 -0500

    DRW

--- 841_Keys_and_Rooms.cpp
+++ 841_Keys_and_Rooms.cpp
@@ -27,3 +27,33 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        vector<bool> seen(rooms.size(), false);
+        seen[0] = true;
+        stack<int> keys;
+        keys.push(0);
+        while(!keys.empty())
+        {
+            int curr_key = keys.top();
+            keys.pop();
+            for(auto key : rooms[curr_key])
+            {
+                if(!seen[key])
+                {
+                    seen[key] = true;
+                    keys.push(key);
+                }
+            }
+        }
+        bool res;
+        for(int i=0; i<seen.size(); i++)
+        {
+            if(seen[i] == false)
+                return false;
+        }
+        return true;
+    }
+};
