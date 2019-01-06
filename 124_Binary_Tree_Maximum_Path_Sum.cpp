a54eec28d96f080cf533c5d0025cbbebb6c15c51
Indrajeet <indrajeet95@gmail.com>
2019-01-06 17:20:06 -0500

    walmart

--- 124_Binary_Tree_Maximum_Path_Sum.cpp
+++ 124_Binary_Tree_Maximum_Path_Sum.cpp
@@ -1,3 +1,8 @@
+IDEA:
+Basically you start at some bottom place and keep moving up and then start going down. You cannot go up again. 
+So recrusively, finish the left hand side and then move to the right. Pass res by reference and keep updating through the whole process
+The helper method tells you 
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -28,5 +33,32 @@ public:
         if(root->val + l + r > res)
             res = root->val + l + r;
         return root->val + max(l, r);
+    }
+};
+WRONG
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        int res = INT_MIN;
+        res = max(res, maxPath(root));
+        return res;
+    }
+    int maxPath(TreeNode* curr)
+    {
+        if(curr == NULL)
+            return 0;
+        else if(curr->left == NULL && curr->right == NULL)
+            return curr->val;
+        else
+            return curr->val + maxPath(curr->left) + maxPath(curr->right);
     }
 };
--- 198_House_Robber.cpp
+++ 198_House_Robber.cpp
@@ -1,3 +1,22 @@
+//Evernote times
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0)
+            return 0;
+        //memo is of size n+1 because memo[i] signifies the max amount of money that you could have stole till this point but not including the current house
+        vector<int> memo(n+1);
+        memo[0] = 0;
+        memo[1] = nums[0];
+        for(int i=1; i<nums.size(); i++)
+        {
+            memo[i+1] = max(memo[i], nums[i] + memo[i-1]);
+        }
+        return memo[nums.size()];
+    }
+};
+//but this also makes sense 
 class Solution {
 public:
     int rob(vector<int>& nums) {
