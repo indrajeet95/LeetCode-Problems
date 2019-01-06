e9a75982a2d62786fc13428e5201c0618ef57116
Indrajeet <indrajeet95@gmail.com>
2019-01-05 19:59:13 -0500

    walmart

--- 873_Length_of_Longest_Fibonacci_Subsequence.cpp
+++ 873_Length_of_Longest_Fibonacci_Subsequence.cpp
@@ -24,3 +24,27 @@ public:
         return res>=3 ? res : 0;
     }
 };
+
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& A) {
+        int res = 0;
+        int n = A.size();
+        unordered_map<int,int> maps;
+        for(int i=0; i<A.size(); i++)
+            maps[A[i]] = i;
+        vector<vector<int>> dp(n, vector<int> (n,2));
+        for(int j=1; j<n; j++)
+        {
+            for(int i=0; i<j; i++)
+            {
+                if(maps.find(A[j] - A[i]) != maps.end() && maps[A[j] - A[i]] < i)
+                    dp[i][j] = max(dp[i][j], 1 + dp[maps[A[j] - A[i]]][i]);
+            }
+        }
+        for(int i=0; i<n; i++)
+            for(int j = 0; j<n; j++)
+                res = max(res, dp[i][j]);
+        return res>2?res:0;
+    }
+};
