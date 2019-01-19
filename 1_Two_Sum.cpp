bb40a4c69e1dfd28ceadfc8f433967298f67c7cf
Indrajeet <indrajeet95@gmail.com>
2019-01-19 14:20:33 -0800

    Shreya

--- 1_Two_Sum.cpp
+++ 1_Two_Sum.cpp
@@ -25,3 +25,26 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unordered_map<int,int> map;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(map.find(target - nums[i]) != map.end())
+            {
+                res.push_back(map[target - nums[i]]);
+                res.push_back(i);
+                return res;
+            }
+            else
+            {
+                map[nums[i]] = i;
+            }
+        }
+        return res;
+    }
+};
