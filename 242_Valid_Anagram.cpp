6419029046d9ce4d0f97ca1b599b07acb728014d
Indrajeet <indrajeet95@gmail.com>
2019-01-29 08:23:20 -0500

    walmart

--- 242_Valid_Anagram.cpp
+++ 242_Valid_Anagram.cpp
@@ -10,3 +10,24 @@ public:
         
     }
 };
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+            return false;
+        int n = s.size();
+        unordered_map<char, int> map;
+        for(int i=0; i<n; i++)
+        {
+            map[s[i]]++;
+            map[t[i]]--;
+        }
+        for(auto vals : map)
+        {
+            if(vals.second)
+                return false;
+        }
+        return true;
+    }
+};
