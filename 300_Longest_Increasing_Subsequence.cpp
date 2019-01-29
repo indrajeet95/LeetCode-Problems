c454148cdd4203882f45f86aca8796cdaaa98f40
Indrajeet <indrajeet95@gmail.com>
2019-01-28 21:22:47 -0500

    walamrt

--- 300_Longest_Increasing_Subsequence.cpp
+++ 300_Longest_Increasing_Subsequence.cpp
@@ -24,3 +24,18 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    //geeksforgeeks
+    //
+int lengthOfLIS(vector<int>& nums) {
+    vector<int> res;
+    for(int i=0; i<nums.size(); i++) {
+        auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
+        if(it==res.end()) res.push_back(nums[i]);
+        else *it = nums[i];
+    }
+    return res.size();
+}    
+};
