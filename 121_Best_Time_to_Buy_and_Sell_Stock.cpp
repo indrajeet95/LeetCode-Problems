bf6bd3e6ff725069de8ce2bc62a20cb73506070c
Indrajeet <indrajeet95@gmail.com>
2019-03-07 10:01:10 -0500

    DP

--- 121_Best_Time_to_Buy_and_Sell_Stock.cpp
+++ 121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -15,3 +15,37 @@ public:
         return max_profit;
     }
 };
+
+March 7th 2019
+O(N2) Solution Brute Force
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int n = prices.size();
+        for(int i=0; i<n; i++) {
+            for(int j = i+1; j<n; j++) {
+                maxProfit = max(maxProfit, prices[j] - prices[i]);
+            }
+        }
+        return maxProfit;
+    }
+};
+
+O(N) Solution Peak - Valley Hint
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+        int n = prices.size();
+        for(int i=0; i<n; i++) {
+            minPrice = min(minPrice, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - minPrice);
+        }
+        return maxProfit;
+    }
+};
+
