922edeb567b3e9f3610b8c575ef5623b437f16a6
Indrajeet <indrajeet95@gmail.com>
2019-03-06 20:57:53 -0500

    DP

---
+++ 256_Paint_House.cpp
@@ -0,0 +1,25 @@
+//O(1) Space Solution
+//O(N) Time Solution
+
+class Solution {
+public:
+    int minCost(vector<vector<int>>& costs) {
+        int n = costs.size();
+        if(!n)
+            return 0;
+        if(n == 1)
+            return min(costs[0][2],min(costs[0][0], costs[0][1]));
+        for(int i=1; i<n; i++) {
+            costs[i][0] = costs[i][0] + min(costs[i-1][1], costs[i-1][2]);
+            costs[i][1] = costs[i][1] + min(costs[i-1][0], costs[i-1][2]);
+            costs[i][2] = costs[i][2] + min(costs[i-1][0], costs[i-1][1]);
+        }
+        return min(costs[n-1][0], min(costs[n-1][1], costs[n-1][2]));
+    }
+};
+
+//O(N2) Space Solution
+//O(N) Time Solution
+
+
+
