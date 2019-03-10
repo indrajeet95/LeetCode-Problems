2051b28d7153f9152cef2c70448e54f82005faad
Indrajeet <indrajeet95@gmail.com>
2019-03-10 06:58:01 -0700

    DP

--- 70_Climbing_Stairs.cpp
+++ 70_Climbing_Stairs.cpp
@@ -1,3 +1,23 @@
+//recursive solution
+//Time limit exceeded
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int res = climbSteps(0,n);
+        return res;
+    }
+    int climbSteps(int i, int j) {
+        if(i > j)
+            return 0;
+        else if(i == j)
+            return 1;
+        else
+            return climbSteps(i+1, j) + climbSteps(i+2, j);
+    }
+};
+
+//Dynamic Programming. 
 class Solution {
 public:
     int climbStairs(int n) {
@@ -9,3 +29,24 @@ public:
         return dp[n];
     }
 };
+
+//fibonaaci sequence
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int first = 1;
+        int second = 1;
+        int third;
+        if(n == 0 || n == 1)
+            return 1;
+        for(int i=2;i<=n;i++) {
+            third = first + second;
+            first = second;
+            second = third;
+        }
+        return third;
+    }
+};
+
+// Can also solve with fibonaaci formula
