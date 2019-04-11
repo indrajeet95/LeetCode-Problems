2754c167606b6f9fdc832a83f1eabbc179ffa44f
Indrajeet <indrajeet95@gmail.com>
2019-04-11 13:31:20 -0400

    greedy

--- 621_Task_Scheduler.cpp
+++ 621_Task_Scheduler.cpp
@@ -21,3 +21,40 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int res = 0;
+        vector<int> map(26,0);
+        for(auto ch : tasks)
+            map[ch - 'A']++;
+        priority_queue<int> holder;
+        for(auto val : map) {
+            if(val > 0)
+                holder.push(val);
+        }
+        while(!holder.empty()) {
+            int i = 0;
+            vector<int> temp;
+            while(i <= n) {
+                if(!holder.empty()) {
+                    if(holder.top() > 1) {
+                        temp.push_back(holder.top() - 1);
+                        holder.pop();
+                    }
+                    else
+                        holder.pop();
+                }
+                res++;
+                if(holder.empty() && temp.empty())
+                    break;
+                i++;
+            }
+            for(int val : temp)
+                holder.push(val);
+        }
+        return res;
+
+    }
+};
