779024fbc63e1a0cfb707c5bdbd83955f7716ae7
Indrajeet <indrajeet95@gmail.com>
2019-04-11 18:41:18 -0400

    graph

--- 207_Course_Schedule.cpp
+++ 207_Course_Schedule.cpp
@@ -1,3 +1,5 @@
+//DFS
+
 class Solution {
 public:
     //finding if there is a cycle in the graph
@@ -48,3 +50,53 @@ public:
     }*/
     
 };
+
+
+//BFS using indegree
+//
+
+class Solution {
+public:
+    //finding if there is a cycle in the graph
+    // if cycle present, return false
+    // else return true
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        graph g = createGraph(numCourses, prerequisites);
+        vector<int> degree = inDegree(g);
+        for(int i=0; i<numCourses; i++) {
+            int j = 0;
+            for(;j < numCourses; j++) {
+                if(!degree[j])
+                    break;
+            }
+            if(j == numCourses)
+                return false;
+            degree[j]--;
+            for(auto v : g[j])
+                degree[v]--;
+        }
+        return true;
+    }
+
+    private:
+    typedef vector<vector<int>> graph;
+    graph createGraph(int numCourses, vector<pair<int,int>> &prerequisites) {
+        graph g(numCourses);
+        for(auto edge : prerequisites) {
+            g[edge.second].push_back(edge.first);
+        }
+        return g;
+    }
+
+    vector<int> inDegree(graph& g) {
+        vector<int> res(g.size(),0);
+        for(auto adj : g) {
+            for(auto v : adj) {
+                res[v]++;
+            }
+        }
+        return res;
+    }
+
+};
+
